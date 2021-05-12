5d8ecabe1f6642e44aa1b52a6a8786b62d559a9b
Mathilde <Mathilde@Mathilde-PC.home>
2021-05-12 22:37:42 +0200

    Operations

---
+++ Exemple6/src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+
+public class Test {
+	// scope number #1
+	
+	public static void main(String[] args) {
+		// scope number #2
+		
+		System.out.println("Welcome ... loading ...");
+		
+		Operations.clientBuyProcess();
+		
+		System.out.println("finishing ....");
+	}
+
+	
+	
+	
+}
+
