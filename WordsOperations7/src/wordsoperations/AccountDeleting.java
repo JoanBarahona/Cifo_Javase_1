a7a8ba9ae2efae98062a28ab3a05a2205bf9b9fe
Mathilde <Mathilde@Mathilde-PC.home>
2021-05-12 23:48:55 +0200

    Deliting

---
+++ WordsOperations7/src/wordsoperations/AccountDeleting.java
@@ -0,0 +1,20 @@
+
+package wordsoperations;
+
+public class AccountDeleting {
+
+	public static void deleteAccount(String user, String email, String password) {
+		
+		// put String to null
+		
+		user = null;
+		email = null;
+		password = null;
+		
+		
+		
+	}
+
+	
+
+}
---
+++ WordsOperations7/src/wordsoperations/Init.java
@@ -0,0 +1,48 @@
+  
+package wordsoperations;
+
+import java.util.Scanner;
+
+public class Init {
+	// scope #1
+	public static void main(String[] args) {
+
+		// scope #2
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Type user (name and surname) to create Account ...");
+		String userToCreateAccount = reader.nextLine();
+
+		
+		
+		String account = AccountManagment.createAccount(userToCreateAccount);
+		
+		//System.out.println(account);
+		
+		String user = Utils.fromStringToUser (account);
+		String email = Utils.fromStringToEmail (account);
+		String password = Utils.fromStringToPassword (account);
+		
+		boolean  isValidated  =  Login.validateAccount (reader, user, password);
+		
+		if (isValidated) {
+			
+			
+			user = AccountUpdating.updateUser(reader, user);
+			email = AccountUpdating.updateEmail(reader, email);
+			password = AccountUpdating.updatePasswod(reader, password);
+			
+			
+			//fake deleting ...
+			AccountDeleting.deleteAccount(user, email, password);
+			
+			isValidated = false;
+		}
+		
+		System.out.println("\nBye bye my friend...");
+		reader.close();
+	}
+
+	
+
+}
